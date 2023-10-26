4811754a96652fcf957f708159734a03dc146f08
Matilda Söderberg <matilda.89@live.se>
2023-10-26 09:28:36 +0200

    Add project files.

---
+++ MvcFlix/Controllers/MovieController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcFlix.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
